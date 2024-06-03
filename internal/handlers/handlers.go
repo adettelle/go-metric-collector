20c8c3a923578ca3af44d5a4bf3e3943a4724b66
Liudmila Litvintseva <l.litvinski@gmail.com>
2024-06-03 22:36:17 +0300

    fix metric post with no value

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 
@@ -26,17 +25,18 @@ func NewMetricApi(storage store.Storage) *MetricApi {
 // CreateMetric adds metric into MemStorage
 // http://localhost:8080/update/counter/someMetric/527
 func (metricApi *MetricApi) CreateMetric(w http.ResponseWriter, r *http.Request) {
-	log.Println("Req:_________________", r)
+	// log.Println("Req:_________________", r)
 	metricNameToSearch := r.PathValue("metric_name")
 	metricValueToSearch := r.PathValue("metric_value")
 	metricTypeToSearch := r.PathValue("metric_type")
-	log.Println("!!!!!!", metricNameToSearch, metricValueToSearch, metricTypeToSearch)
+	// log.Println("!!!!!!", metricNameToSearch, metricValueToSearch, metricTypeToSearch)
 
 	switch {
 	case metricTypeToSearch == "gauge":
 		value, err := strconv.ParseFloat(metricValueToSearch, 32)
 		if err != nil {
-			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
 		metricApi.Storage.AddGaugeMetric(metricNameToSearch, value)
 
@@ -46,21 +46,18 @@ func (metricApi *MetricApi) CreateMetric(w http.ResponseWriter, r *http.Request)
 	case metricTypeToSearch == "counter":
 		value, err := strconv.ParseInt(metricValueToSearch, 10, 64)
 		if err != nil {
-			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
 		metricApi.Storage.AddCounterMetric(metricNameToSearch, value)
 
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Created"))
 		fmt.Println(metricApi.Storage) // {map[] map[someMetric:[527]]}
-		// что это такое в начале стрки????
-		// &{{{0 0} 0 0 {{} 0} {{} 0}} map[Alloc:827680 BuckHashSys:6919...] map[PollCount:10]}
 
-	case metricTypeToSearch == "":
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Metric name is empty"))
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("No such metric"))
+		return
 	}
 }
