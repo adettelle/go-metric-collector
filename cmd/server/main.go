f19e2c504c2eb7c73f2c71a12bf448ae61a89e5b
adettelle <113937974+adettelle@users.noreply.github.com>
2024-06-07 22:46:44 +0300

    implement flags on server and agent

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/adettelle/go-metric-collector/internal/handlers"
 	store "github.com/adettelle/go-metric-collector/internal/storage"
 	"github.com/go-chi/chi/v5"
 )
 
+type NetAddress struct {
+	Host string
+	Port int
+}
+
 func main() {
+	addr := flag.String("a", "localhost:8080", "Net address localhost:port")
+	flag.Parse()
+	ensureAddrFLagIsCorrect(*addr)
+
 	ms := store.NewMemStorage()
 	mAPI := handlers.NewMetricAPI(ms)
 
@@ -20,8 +33,22 @@ func main() {
 	r.Get("/value/{metric_type}/{metric_name}", mAPI.GetMetricByValue)
 	r.Get("/", mAPI.GetAllMetrics)
 
-	err := http.ListenAndServe(`:8080`, r)
+	fmt.Printf("Starting server on %s\n", *addr)
+
+	err := http.ListenAndServe(*addr, r) // `:8080`
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func ensureAddrFLagIsCorrect(addr string) {
+	_, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = strconv.Atoi(port)
+	if err != nil {
+		log.Fatal(fmt.Errorf("invalid port: '%s'", port))
 	}
 }
