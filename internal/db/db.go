3f4e36c7a34cc72dc687bb75dca6681e7ccc4b54
Liudmila Litvintseva <l.litvinski@gmail.com>
2024-07-18 19:04:20 +0300

    implement data signature in in adding one metric

--- internal/db/db.go
+++ internal/db/db.go
@@ -14,7 +14,7 @@ func CreateTable(db *sql.DB, ctx context.Context) error { // metric_type_enum
 	sqlStqtement := "create table if not exists metric" +
 		"(id serial primary key , metric_type text not null," +
 		"metric_id varchar(30) not null, value double precision not null default 0," +
-		"delta integer not null default 0, created_at timestamp not null default now());"
+		"delta bigint not null default 0, created_at timestamp not null default now());" // integer
 
 	_, err := db.ExecContext(ctx, sqlStqtement)
 	if err != nil {
